2ebf320f1011c5fdb7a023225547a6a73be3419b
Elvan H <elvanhd@gmail.com>
2022-01-18 17:33:32 +0700

    menangani error validasi dan id mongodb

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,27 +1,29 @@
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
 function errorHandler(err, req, res, next) {
+  let error = { ...err };
+
   err.statusCode = err.statusCode || 500;
   err.message = err.message || 'Something Went Wrong';
 
-  if (NODE_ENV === 'development') {
-    return res.status(err.statusCode).json({
-      success: false,
-      status: err.statusCode,
-      message: err.message,
-      stack: err.stack,
-    });
-  } else if (NODE_ENV === 'production') {
-    let error = { ...err };
-
-    error.message = err.message;
-
-    return res.status(err.statusCode).json({
-      success: false,
-      status: err.statusCode,
-      message: err.message || 'Something Went Wrong',
-    });
+  if (err.name === 'CastError') {
+    err.statusCode = 400;
+    err.message = `Resource with invalid path ${err.path} was not found. `;
   }
+
+  if (err.name === 'ValidationError') {
+    const messages = Object.values(err.errors).map(({ message }) => message);
+
+    err.statusCode = 400;
+    err.message = messages.join(', ');
+  }
+
+  return res.status(err.statusCode).json({
+    success: false,
+    status: err.statusCode,
+    message: err.message,
+    stack: NODE_ENV === 'production' ? null : err.stack,
+  });
 }
 
 module.exports = errorHandler;
