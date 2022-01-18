59a93bb24a6b5a02f50590b812e5ac9804a9db8c
Elvan H <elvanhd@gmail.com>
2022-01-18 16:45:36 +0700

    memisahkan error development dan production

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,12 +1,27 @@
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
 function errorHandler(err, req, res, next) {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || 'Something Went Wrong';
 
-  res.status(err.statusCode).json({
-    success: false,
-    message: err.message,
-    stack: err.stack,
-  });
+  if (NODE_ENV === 'development') {
+    return res.status(err.statusCode).json({
+      success: false,
+      status: err.statusCode,
+      message: err.message,
+      stack: err.stack,
+    });
+  } else if (NODE_ENV === 'production') {
+    let error = { ...err };
+
+    error.message = err.message;
+
+    return res.status(err.statusCode).json({
+      success: false,
+      status: err.statusCode,
+      message: err.message || 'Something Went Wrong',
+    });
+  }
 }
 
 module.exports = errorHandler;
