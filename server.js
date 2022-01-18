36186c4a20ee101a53a067a428d581fe0439625b
Elvan H <elvanhd@gmail.com>
2022-01-18 17:15:31 +0700

    menangani unhandled promise rejection

--- server.js
+++ server.js
@@ -14,6 +14,17 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 connectToDatabase();
 
 // Start the server
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running on port ${port} in ${NODE_ENV} mode`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(`Error name: ${err.name}`);
+  console.log(`Error message: ${err.message}`);
+  console.log('Unhandled Rejection. Shutting down...');
+
+  // Close server and exit process
+  server.close(() => {
+    process.exit(1);
+  });
 });
